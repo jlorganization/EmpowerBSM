814c72a829dc46667f16494764014fef00797d23
callej <callej@9SZ5VN2>
2023-05-16 14:09:19 -0400

    Add project files.

---
+++ jcempower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jcempower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("jcempower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D6496DF2-E612-48E4-A8CA-510DF4B07E01")]
+[assembly: AssemblyVersion("1.0.0.0")]
